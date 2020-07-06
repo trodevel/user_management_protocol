ec61c88b850a6b015858d0cf6f7debc949b4058a
trodevel <trodevel@gmail.com>
2020-07-06 03:38:31 +0200

    PHP: updated tests

--- tests/test_SetUserInfoResponse.php
+++ tests/test_SetUserInfoResponse.php
@@ -1,18 +1,25 @@
 <?php
 
-// $Revision: 12295 $ $Date:: 2019-10-30 #$ $Author: serge $
+// $Revision: 13367 $ $Date:: 2020-07-06 #$ $Author: serge $
 
-require_once '../user_management_protocol.php';
-require_once '../response_parser.php';
+require_once '../protocol.php';
+require_once '../parser.php';
 require_once '../html_helper.php';
 
 {
     $resp  =
     'user_management/SetUserInfoResponse;';
 
-    $parsed = \user_management_protocol\ResponseParser::parse( $resp );
+    $parsed = \user_management_protocol\Parser::parse( $resp );
 
-    echo "parsed: " . \user_management_protocol\to_html( $parsed ) . "\n";
+    if( $parsed )
+    {
+        echo "parsed: " . \user_management_protocol\to_html( $parsed ) . "\n";
+    }
+    else
+    {
+        echo "ERROR: cannot parse\n";
+    }
 }
 
 ?>
